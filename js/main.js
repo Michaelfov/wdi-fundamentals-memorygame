ae8d0ed47f003ef45243b1b0c478d1309e85948b
Josephine <Josephine@zhangyijiedeMacBook-Pro.local>
2019-03-12 20:30:58 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 console.log("Up and running!");
-var cardOne = "Queen";
-var cardTwo = "King";
-var cardThree = "Queen";
-var cardFour = "King";
+var cards =["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0]
+var cardTwo = cards[1]
+cardsInPlay.push("cardOne");
+cardsInPlay.push("cardTwo");
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+if (cardsInPlay[0]===cardsInPlay[1])
+{
+	alert("You find the match");
+}
+else {
+	alert("Sorry, please try again");
+}
