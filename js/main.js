78583cf4044fd61ea15a4beb21f36156f26cb289
Josephine <Josephine@zhangyijiedeMacBook-Pro.local>
2019-03-17 19:28:45 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -21,6 +21,27 @@ var cards = [
 }
 ];
 var cardsInPlay = []; 
+var createBoard = function() {
+	for(var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+var flipCard = function() {	
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
+	console.log("User flipped" + " " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	if (cardsInPlay.length == 2) {
+		checkForMatch()
+	}
+};
 var checkForMatch = function() {
 	if(cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!")
@@ -30,12 +51,5 @@ var checkForMatch = function() {
 		alert("Sorry, pleae try again");
 	}
 };
-var flipCard = function(cardId) {	
-	console.log("User flipped" + " " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
-}
-flipCard(0);
-flipCard(2);
-checkForMatch()
+
+createBoard();
